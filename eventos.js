69651aabbb689cf4287d53c34090f2a8142b986d
valentina <m.valentinabrites@gmail.com>
2024-09-02 02:03:02 -0300

    Entrega pregunta 5

--- eventos.js
+++ eventos.js
@@ -3,4 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
   container.addEventListener("click", function () {
     alert("Hola! Soy el div");
   });
+  let saludar = this.getElementById("saludar");
+  saludar.addEventListener("click", function (e) {
+    e.stopPropagation();
+    alert("Hola!");
+  });
 });
